042d2e07a3f3a7cace2f7a2b7071b1b13a9d0aee
Andrew <andrewhinkson88@gmail.com>
2020-01-26 02:48:09 -0500

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-@Injectable(
-
-)
+@Injectable()
 
 export class ProductService {
 
