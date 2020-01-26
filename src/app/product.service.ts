200cd05587f1dafba0ce318d50f9f1cd9e71617d
Andrew <andrewhinkson88@gmail.com>
2020-01-26 02:18:05 -0500

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 @Injectable()
+
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
 
-  private class 
+ 
   constructor(private _http:Http) { }
 
   getAlbum(id: number){
