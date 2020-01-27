112a4723636b0471736c1f074801feeaed290b1e
Andrew <andrewhinkson88@gmail.com>
2020-01-26 19:45:32 -0500

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
     
     ProductDescriptionComponent,
     
-    ProductTracklistingComponent
+    ProductTracklistingComponent,
+    
+    ProductListComponent
     
   
   ],
